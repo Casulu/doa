e6d0b05ca87e08a90ce11e8394c18566be7cd860
Casulu <rasmuslyxell@gmail.com>
2020-03-19 16:29:11 +0100

    Tog bort Valgrindtester och tog bort array_1d & table2 från komilering

--- ou5/kod/make.sh
+++ ou5/kod/make.sh
@@ -1,2 +1,2 @@
-gcc -g -std=c99 -Wall -o is_connected -I ../../datastructures-v1.0.8.2/include/ graph.c is_connected.c ../../datastructures-v1.0.8.2/src/list/list.c ../../datastructures-v1.0.8.2/src/queue/queue.c ../../datastructures-v1.0.8.2/src/dlist/dlist.c ../../datastructures-v1.0.8.2/src/array_1d/array_1d.c ../../datastructures-v1.0.8.2/src/table/table2.c
+gcc -g -std=c99 -Wall -o is_connected -I ../../datastructures-v1.0.8.2/include/ graph.c is_connected.c ../../datastructures-v1.0.8.2/src/list/list.c ../../datastructures-v1.0.8.2/src/queue/queue.c ../../datastructures-v1.0.8.2/src/dlist/dlist.c
 
