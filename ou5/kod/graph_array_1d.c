cf316f32880a4bc3908262f3356f6e80835166b6
Casulu <rasmuslyxell@gmail.com>
2020-03-19 16:26:11 +0100

    Ändrade till fysiska fält i graph.c och fixade en massa buggar i is_connected.c. Kommenterade allt.

--- ou5/kod/graph_array_1d.c
+++ ou5/kod/graph_array_1d.c
@@ -1,20 +1,34 @@
 #include <stdbool.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdio.h>
+#include "array_1d.h"
 #include "graph.h"
 #include "dlist.h"
 
 /*
- * A graph stores nodes and edges of a directed graph.
- * After use, the function graph_kill() must
+ * Declaration of a generic graph for the "Datastructures and
+ * algorithms" courses at the Department of Computing Science, Umea
+ * University. The graph stores nodes and edges of a directed or
+ * undirected graph. After use, the function graph_kill() must
  * be called to de-allocate the dynamic memory used by the graph
  * itself. The de-allocation of any dynamic memory allocated for the
  * node names is the responsibility of the user of the graph.
  *
- * Author: Rasmus Lyxell (c19rll@cs.umu.se)
+ * Author: Niclas Borlin (niclas.borlin@cs.umu.se)
  *
  * Version information:
  *   v1.0  2019-02-21: First public version.
+
+ *   v1.01 2019-02-26: Modified include directives for header files
+ *                     from the codebase to use "" to handle case when
+ *                     student stores all header files in the current
+ *                     directory. See
+ *                     https://stackoverflow.com/questions/21593/what-is-the-difference-between-include-filename-and-include-filename.
+ *   v1.02 2019-03-01: Doc update in graph_node_is_seen().
+ *   v1.1  2019-03-06: Changed several const node * to node *.
+ *                     Fixed doc bug to state that any dynamic memory allocated
+ *                     to the node NAMES is the resposibility of the user.
  */
 
 // ====================== PUBLIC DATA TYPES ==========================
@@ -26,9 +40,9 @@ struct node {
 };
 
 struct graph{
-	node **nodes;
+	array_1d *nodes;
 	int numNodes;
-	int numEdges;
+	int numNeighbours;
 };
 
 // =================== NODE COMPARISON FUNCTION ======================
@@ -51,7 +65,7 @@ bool nodes_are_equal(const node *n1,const node *n2)
 	}
 }
 
-// =================== GRAPH STRUCTURE ======================
+// =================== GRAPH STRUCTURE INTERFACE ======================
 
 /**
  * graph_empty() - Create an empty graph.
@@ -63,8 +77,19 @@ graph *graph_empty(int max_nodes)
 {
 	graph *g = calloc(1, sizeof(graph));
 	g->numNodes = 0;
-	g->nodes = calloc(max_nodes, sizeof(node*));
+	g->nodes = array_1d_create(0, max_nodes, NULL);
 	return g;
+}
+
+/**
+ * graph_is_empty() - Check if a graph is empty, i.e. has no nodes.
+ * @g: Graph to check.
+ *
+ * Returns: True if graph is empty, otherwise false.
+ */
+bool graph_is_empty(const graph *g)
+{
+	return array_1d_inspect_value(g->nodes, 0) == NULL;
 }
 
 /**
@@ -75,7 +100,7 @@ graph *graph_empty(int max_nodes)
  */
 bool graph_has_edges(const graph *g)
 {
-    return g->numEdges > 0;
+    return g->numNeighbours > 0;
 }
 
 /**
@@ -91,11 +116,12 @@ bool graph_has_edges(const graph *g)
 graph *graph_insert_node(graph *g, const char *s)
 {
     node *n = malloc(sizeof(*n));
+
     n->seen = false;
     n->label = calloc(strlen(s) + 1, sizeof(char));
     strcpy(n->label, s);
     n->neighbours = dlist_empty(NULL);
-    g->nodes[g->numNodes] = n;
+    array_1d_set_value(g->nodes, n, g->numNodes);
     g->numNodes++;
     return g;
 }
@@ -110,7 +136,7 @@ graph *graph_insert_node(graph *g, const char *s)
 node *graph_find_node(const graph *g, const char *s)
 {
     for (int i = 0; i < g->numNodes; ++i) {
-        node *currNode = g->nodes[i];
+        node *currNode = array_1d_inspect_value(g->nodes, i);
         if(strcmp(s, currNode->label) == 0){
             return currNode;
         }
@@ -153,7 +179,7 @@ graph *graph_node_set_seen(graph *g, node *n, bool seen)
 graph *graph_reset_seen(graph *g)
 {
     for (int i = 0; i < g->numNodes; ++i) {
-        node *currNode = g->nodes[i];
+        node *currNode = array_1d_inspect_value(g->nodes, i);
         currNode->seen = false;
     }
     return g;
@@ -172,8 +198,21 @@ graph *graph_reset_seen(graph *g)
 graph *graph_insert_edge(graph *g, node *n1, node *n2)
 {
     dlist_insert(n1->neighbours, n2, dlist_first(n1->neighbours));
-    g->numEdges++;
+    g->numNeighbours++;
     return g;
+}
+
+/**
+ * graph_choose_node() - Return an arbitrary node from the graph.
+ * @g: Graph to inspect.
+ *
+ * Returns: A pointer to an arbitrayry node.
+ *
+ * NOTE: The return value is undefined for an empty graph.
+ */
+node *graph_choose_node(const graph *g)
+{
+    return array_1d_inspect_value(g->nodes, g->numNodes - 1);
 }
 
 /**
@@ -206,11 +245,11 @@ dlist *graph_neighbours(const graph *g,const node *n)
 void graph_kill(graph *g)
 {
     for (int i = g->numNodes-1; i >= 0; i--) {
-        node *currNode = g->nodes[i];
+        node *currNode = array_1d_inspect_value(g->nodes, i);
         free(currNode->label);
         dlist_kill(currNode->neighbours);
         free(currNode);
     }
-    free(g->nodes);
+    array_1d_kill(g->nodes);
     free(g);
 }
--- ou5/kod/is_connected.c
+++ ou5/kod/is_connected.c
@@ -5,18 +5,21 @@
 #include <errno.h>
 #include <ctype.h>
 #include "graph.h"
-#include "array_1d.h"
 #include "queue.h"
 
 #define MAX_LINE_LENGTH 300
 #define MAX_NODE_LENGTH 40
+#define COMMENT_SIGN '#'
 
-/* Return position of first non-whitespace character or -1 if only
-white-space is found. */
+/**
+ * Get first non-whitespace index in string.
+ * @param s - String to get index from
+ * @return Index of first non-whitespace character in string
+ */
 int firstNonWhiteSpace(const char *s)
 {
     int i = 0; // Start at first char.
-// Advance until we hit EOL as long as we're loooking at white-space.
+    // Advance until we hit EOL as long as we're loooking at white-space.
     while (s[i] && isspace(s[i])) {
         i++;
     }
@@ -28,12 +31,17 @@ int firstNonWhiteSpace(const char *s)
 }
 /* Return position of last non-whitespace character or -1 if only
 white-space is found. */
+/**
+ * Gets the last non-whitespace character in a string.
+ * @param s - String to get index from
+ * @return Index of last non-whitespace character in string or -1 if reading failed
+ */
 int lastNonWhiteSpace(const char *s)
 {
-// Start at last char.
+    // Start at last char.
     int i = strlen(s) - 1;
-// Move back until we hit beginning-of-line as long as we're
-// loooking at white-space.
+    // Move back until we hit beginning-of-line as long as we're
+    // loooking at white-space.
     while (i >= 0 && isspace(s[i])) {
         i--;
     }
@@ -44,24 +52,36 @@ int lastNonWhiteSpace(const char *s)
     }
 }
 
-/* Return true if s only contains whitespace */
+/**
+ *  Checks if string only contains whitespace
+ * @param s - String to check
+ * @return True if string is blank. False otherwise.
+ */
 bool lineIsBlank(const char *s)
 {
-// Line is blank if it only contained white-space chars.
+    // Line is blank if it only contained white-space chars.
     return firstNonWhiteSpace(s) < 0;
 }
 
-/* Return true if s is a comment line, i.e. first non-whitespc char is '#' */
+/**
+ * Checks if string given is considered a comment
+ * @param s - String to check
+ * @return True if string is comment. False otherwise.
+ */
 bool lineIsComment(const char *s)
 {
     int i = firstNonWhiteSpace(s);
-    return (i >= 0 && s[i] == '#');
+    return (i >= 0 && s[i] == COMMENT_SIGN);
 }
 
-// Returns the index of the first comment sign or the last non-whitespace if no comment sign is found
+/**
+ * Gets the index of the first comment sign.
+ * @param s - String to get index from
+ * @return Returns the index of the first comment sign if one was found or the index after the last nonwhitespace
+ */
 int firstCommentSign(const char *s){
     int i = 0;
-    while(s[i] != '#' && s[i]){
+    while(s[i] != COMMENT_SIGN && s[i]){
         i++;
     }
 
@@ -74,12 +94,17 @@ int firstCommentSign(const char *s){
 
 }
 
+/**
+ * Gets the last non-whitespace character in a line while ignoring comments
+ * @param s - String to get index from
+ * @return Index of last non-whitespace character ignoring comments in string or -1 if reading failed
+ */
 int lastNonWhiteSpaceNoComment(const char *s)
 {
-// Start at last char.
+    // Start at last char.
     int i = firstCommentSign(s) - 1;
-// Move back until we hit beginning-of-line as long as we're
-// looking at white-space.
+    // Move back until we hit beginning-of-line as long as we're
+    // looking at white-space.
     while (i >= 0 && isspace(s[i])) {
         i--;
     }
@@ -90,7 +115,11 @@ int lastNonWhiteSpaceNoComment(const char *s)
     }
 }
 
-/*Opens a file for reading while checking for errors. Returns the opened FILE pointer */
+/**
+ *  Reads given path for opening. Exits via exit(EXIT_FAILURE) if reading failed
+ * @param name - Path of file to read
+ * @return An opened filestream to the given path.
+ */
 FILE *readFile(const char *name){
     FILE *in;
     /* open for reading */
@@ -99,7 +128,7 @@ FILE *readFile(const char *name){
     if (in == NULL) {
         fprintf(stderr, "ERROR: Failed to open %s for reading: %s\n",
                 name, strerror(errno));
-        exit(1);
+        exit(EXIT_FAILURE);
     }
     return in;
 }
@@ -114,9 +143,10 @@ graph *interpretMap(FILE *map)
 {
     graph *g;
     char lineBuffer[MAX_LINE_LENGTH];
+    bool reachedEdge;
+    bool badLine;
+
     bool numIsRead = false;
-    bool reachedEdge = false;
-    bool badLine = false;
     int numEdges = 0;
 
     //This block looks for the first non-ignored line and grabs the number of lines.
@@ -164,128 +194,187 @@ graph *interpretMap(FILE *map)
             int offset = firstNonWhiteSpace(lineBuffer); //Offset for the index of the line being read.
             int j = 0; //Index for the line being read
             int h = 0; //Index for the label buffer
-            //Start reading the read line until the max length is passed, until whitespace is reached, or until the end of the line
-            while (lineBuffer[j + offset] && lineBuffer[j + offset] != ' ' && h <= MAX_NODE_LENGTH) {
+
+            //Start reading the read line until the max node length is passed, until whitespace is reached, or until the end of the line
+            while (lineBuffer[j + offset] && !isspace(lineBuffer[j + offset]) && h <= MAX_NODE_LENGTH && lineBuffer[j + offset] != COMMENT_SIGN) {
                 if(isalnum(lineBuffer[j + offset])){
                     lblBuff[h] = lineBuffer[j + offset];
                     j++;
                     h++;
                 } else{
-                    //Print error and exit if a non-alphanumerical character is read outside of a comment
+                    //Print error and exit if a non-alphanumerical character is read
                     fprintf(stderr, "ERROR: Edge line\n%scontained non alphanumerical character '%c'\n", lineBuffer, lineBuffer[j + offset]);
                     exit(EXIT_FAILURE);
                 }
             }
             if(h > MAX_NODE_LENGTH){
+                //Print error and exit if node is longer than specified max length
                 fprintf(stderr, "ERROR: First node in line:\n%scontains more than %d characters", lineBuffer, MAX_NODE_LENGTH);
                 exit(EXIT_FAILURE);
             }
+            if(lineBuffer[j + offset] != ' '){
+                //Print error and exit if first node is last node on line
+                fprintf(stderr, "ERROR: Separator other than space in Line:\n%s", lineBuffer);
+                exit(EXIT_FAILURE);
+            }
+            if(j + offset - 1 == lastNonWhiteSpaceNoComment(lineBuffer)){
+                //Print error and exit if line buffer index reached last readable position after one node
+                fprintf(stderr, "ERROR: Line:\n %s only contains one node", lineBuffer);
+                exit(EXIT_FAILURE);
+            }
+            //End string and copy to first label
             lblBuff[h] = '\0';
             strcpy(lbl1, lblBuff);
+            //Insert label as node if not already in graph
             if(graph_find_node(g, lbl1) == NULL){
                 graph_insert_node(g, lbl1);
             }
+            //Reset label buffer index and loop line buffer index until a non-space character is found
             h = 0;
             while (lineBuffer[j + offset] == ' ') {
                 j++;
             }
-            if(lastNonWhiteSpaceNoComment(lineBuffer) == j + offset){
-                fprintf(stderr, "ERROR: Line:\n %s only contains one node", lineBuffer);
-                exit(EXIT_FAILURE);
-            }
 
-            while (lineBuffer[j + offset] && lineBuffer[j + offset] != ' ' && lineBuffer[j + offset] != '#' && h <= MAX_NODE_LENGTH) {
+            //Keep reading line buffer until the max node length is passed, until whitespace is reached, until a comment is reached, or until the end of the line
+            while (lineBuffer[j + offset] && !isspace(lineBuffer[j + offset]) && lineBuffer[j + offset] != COMMENT_SIGN && h <= MAX_NODE_LENGTH) {
                 if(isalnum(lineBuffer[j + offset])){
                     lblBuff[h] = lineBuffer[j + offset];
                     j++;
                     h++;
                 } else{
+                    //Print error and exit if a non-alphanumerical character is read
                     fprintf(stderr, "ERROR: Edge line:\n%scontained non alphanumerical character '%c'\n", lineBuffer, lineBuffer[j + offset]);
                     exit(EXIT_FAILURE);
                 }
             }
             if(h > MAX_NODE_LENGTH){
+                //Print error and exit if node is longer than specified max length
                 fprintf(stderr, "ERROR: Second node in line '%s' contains more than %d characters", lineBuffer, MAX_NODE_LENGTH);
                 exit(EXIT_FAILURE);
             }
+            if(j + offset - 1 != lastNonWhiteSpaceNoComment(lineBuffer)){
+                //Print error and exit if line buffer index reached last readable position after one node
+                fprintf(stderr, "ERROR: Line:\n %s contains more than two node", lineBuffer);
+                exit(EXIT_FAILURE);
+            }
+            //End string and copy to first label
             lblBuff[h] = '\0';
             strcpy(lbl2, lblBuff);
+            //Free label buffer. We are done with it.
             free(lblBuff);
 
+            //Insert label as node if not already in graph
             if(graph_find_node(g, lbl2) == NULL){
                 graph_insert_node(g, lbl2);
             }
+
+            //Create pointers for the nodes corresponding to the read labels
             node *n1 = graph_find_node(g, lbl1);
             node *n2 = graph_find_node(g, lbl2);
 
+            //Variables for looping through first nodes neighbours
             dlist *lbl1Neighbours = graph_neighbours(g, n1);
             dlist_pos pos = dlist_first(lbl1Neighbours);
             bool duplicate = false;
-            while(!dlist_is_end(lbl1Neighbours, pos)){
+
+            //Loop through  the first nodes neighbours and check if an edge between the two nodes already exists
+            while(!dlist_is_end(lbl1Neighbours, pos) && !duplicate){
                 if(nodes_are_equal(dlist_inspect(lbl1Neighbours, pos), n2)){
                     duplicate = true;
                 }
                 pos = dlist_next(lbl1Neighbours, pos);
             }
+            //Done with list
             dlist_kill(lbl1Neighbours);
+
 
             if(!duplicate){
                 graph_insert_edge(g, n1, n2);
                 actualNumEdges++;
             } else{
+                //Print error and exit if a duplicate edge was found
                 fprintf(stderr, "ERROR: File had duplicate edges of '%s %s'\n", lbl1, lbl2);
                 exit(EXIT_FAILURE);
             }
+            //Done with labels
             free(lbl1);
             free(lbl2);
 
         }
         if(actualNumEdges > numEdges){
             fprintf(stderr, "ERROR: File stated %d but had at least %d\n", numEdges, actualNumEdges);
-            exit(EXIT_FAILURE);;
+            exit(EXIT_FAILURE);
         }
     }
 
     if(actualNumEdges < numEdges){
         fprintf(stderr, "ERROR: File stated %d edges but file had %d\n", numEdges, actualNumEdges);
-        exit(EXIT_FAILURE);;
+        exit(EXIT_FAILURE);
     }
     return g;
 
 }
 
+/**
+ * Performs a breadth first search for a given node from another give node in a given graph
+ * @param g - The graph to be searched in
+ * @param src - The node to start traversing from
+ * @param dest - The node to search for
+ * @return - True if dest is found. False otherwise
+ */
 bool find_path(graph *g, node *src, node *dest){
+    //Node is found if both nodes are the same
     if(nodes_are_equal(src, dest)){
         return true;
     }
-    queue *q = queue_empty(NULL);
-    dlist *neighbourSet;
-    graph_node_set_seen(g, src, true);
-    q = queue_enqueue(q, src);
-    while(!queue_is_empty(q)){
-        node *currNode = queue_front(q);
-        q = queue_dequeue(q);
-        neighbourSet = graph_neighbours(g, currNode);
-        dlist_pos pos = dlist_first(neighbourSet);
-        while(!dlist_is_end(neighbourSet, pos)){
-            node *currNeighbour = dlist_inspect(neighbourSet, pos);
-            if(nodes_are_equal(currNeighbour, dest)){
-                dlist_kill(neighbourSet);
-                queue_kill(q);
-                return true;
+
+    if(graph_has_edges(g)){
+        //Create helper queue
+        queue *q = queue_empty(NULL);
+        //Set start node as seen and put it in queue
+        graph_node_set_seen(g, src, true);
+        q = queue_enqueue(q, src);
+        dlist *neighbourSet;
+
+        //Breadth first search until node is found or all nodes have been visited
+        while(!queue_is_empty(q)){
+            //Take first node in queue
+            node *currNode = queue_front(q);
+            q = queue_dequeue(q);
+            //Loop through neighbours and check for match with destination node. Add to queue and set as seen if not already seen
+            neighbourSet = graph_neighbours(g, currNode);
+            dlist_pos pos = dlist_first(neighbourSet);
+            while(!dlist_is_end(neighbourSet, pos)){
+                node *currNeighbour = dlist_inspect(neighbourSet, pos);
+                if(nodes_are_equal(currNeighbour, dest)){
+                    //Kill everything early, reset nodes seen status, and return true if destination node is found
+                    dlist_kill(neighbourSet);
+                    queue_kill(q);
+                    graph_reset_seen(g);
+                    return true;
+                }
+                if(!graph_node_is_seen(g, currNeighbour)){
+                    graph_node_set_seen(g, currNeighbour, true);
+                    q = queue_enqueue(q, currNeighbour);
+                }
+                pos = dlist_next(neighbourSet, pos);
             }
-            if(!graph_node_is_seen(g, currNeighbour)){
-                graph_node_set_seen(g, currNeighbour, true);
-                q = queue_enqueue(q, currNeighbour);
-            }
-            pos = dlist_next(neighbourSet, pos);
+            //Done with this neighbour set. Free
+            dlist_kill(neighbourSet);
         }
-        dlist_kill(neighbourSet);
+        //Done with queue. Reset all nodes seen status and free queue
+        queue_kill(q);
+        graph_reset_seen(g);
+        return false;
+    } else{
+        return false;
     }
-    queue_kill(q);
-    return false;
 }
 
+/**
+ * Reads the file given in argv as a graph and takes input from user to search for paths in the file. Prints the result
+ * and asks again until 'quit' is inputted.
+ */
 int main(int argc, char *argv[]){
     if(argc != 2){
         fprintf(stderr, "Number of arguments exceeded 1");
@@ -296,60 +385,73 @@ int main(int argc, char *argv[]){
     graph *g = interpretMap(map);
     fclose(map);
 
-    char input[83];
-    char start[41];
-    char goal[41];
+    char input[MAX_NODE_LENGTH + 2];
+    char srcLbl[MAX_NODE_LENGTH + 1];
+    char destLbl[MAX_NODE_LENGTH + 1];
 
+    //Loops until user inputs
     do{
+        //Clear last buffer to avoid infinite loops
+        strcpy(input, "");
         printf("Enter origin and destination (quit to exit): ");
-        scanf("%[^\n]%*c", input);
+        //Scan entire entered line and discard the newline
+        scanf("%[^\n]", input);
+        scanf("%*c");
 
+        //Don't search if 'quit' was inputted
         if(strcmp(input, "quit") != 0){
             int lastNW = lastNonWhiteSpace(input);
             int i = 0;
-            while(!isspace(input[i]) && input[i] != '\0'){
-                start[i] = input[i];
+            //Copy input to label until end of input or until space is encountered
+            while(input[i] != ' ' && input[i]){
+                srcLbl[i] = input[i];
                 i++;
             }
-            if(lastNW != i-1){
-                start[i] = '\0';
+            //End string
+            srcLbl[i] = '\0';
+            //If there is more to read, keep reading
+            if(i-1 != lastNW && i-1 != strlen(input)){
                 i++;
                 int j = 0;
-                while(!isspace(input[i]) && input[i] != '\0'){
-                    goal[j] = input[i];
+                while(!isspace(input[i]) && input[i]){
+                    destLbl[j] = input[i];
                     i++;
                     j++;
                 }
-                goal[j] = '\0';
-                if(lastNW == i-1){
-                    node *src = graph_find_node(g, start);
-                    node *dest = graph_find_node(g, goal);
+                destLbl[j] = '\0';
+                //If we have reached the end of the input (or last whitespace) after second node
+                if(i-1 == lastNW || i-1 == strlen(input)){
+                    node *src = graph_find_node(g, srcLbl);
+                    node *dest = graph_find_node(g, destLbl);
                     if(src == NULL) {
-                        printf("The node '%s' does not exist in the given map.\n\n", start);
+                        printf("The node '%s' does not exist in the given map.\n\n", srcLbl);
                     } else if(dest == NULL){
-                        printf("The node '%s' does not exist in the given map.\n\n", goal);
+                        printf("The node '%s' does not exist in the given map.\n\n", destLbl);
                     } else{
                         if(find_path(g, src, dest)){
-                            printf("There is a path from %s to %s.\n\n", start, goal);
+                            printf("There is a path from %s to %s.\n\n", srcLbl, destLbl);
                         } else{
-                            printf("There is no path from %s to %s.\n\n", start, goal);
+                            printf("There is no path from %s to %s.\n\n", srcLbl, destLbl);
                         }
                     }
                 } else{
-                    printf("Please enter exactly two nodes in the format 'n1 n2'.\n\n");
+                    //Prints if one or zero nodes were entered
+                    printf("Please enter exactly two nodes in the format 'n1 n2' (separated with one space).\n\n");
                 }
 
             } else{
-                printf("Please enter exactly two nodesin the format 'n1 n2'.\n\n");
+                //Prints if more than two nodes were entered
+                printf("Please enter exactly two nodes in the format 'n1 n2' (separated with one space).\n\n");
             }
         }
 
 
     }while(strcmp(input, "quit") != 0);
 
+    //Done with graph
     graph_kill(g);
-
     printf("Normal exit.\n");
+
     return 0;
 }
 
